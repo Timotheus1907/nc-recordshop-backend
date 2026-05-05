a425b8b3cf415a7d2d67c569b3382391f97415cc
Timothy <hawkinsjtimothy@gmail.com>
2026-05-05 10:48:42 +0100

    Added Album Repository layer

--- nc-recordshop-backend/Model/Album.cs
+++ nc-recordshop-backend/Model/Album.cs
@@ -1,6 +1,16 @@
 ﻿namespace nc_recordshop_backend.Model
 {
+    // public enum Genre { }
+
     public class Album
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Artist { get; set; } = "";
+        // string or enum
+        public string Genre { get; set; } = "";
+        // string or DateTime
+        public DateTime ReleaseDate { get; set; }
     }
 }
---
+++ nc-recordshop-backend/Repository/AlbumRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace nc_recordshop_backend.Repository
+{
+    public class AlbumRepository
+    {
+        // constructor with a private db field here
+    }
+}
